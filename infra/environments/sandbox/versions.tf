78c7524f92abdb591c926c7d5f5064bff7527320
Remmusss <tdmq207@gmail.com>
2026-06-26 13:31:35 +0700

    chore(infra): rename state bucket to us-east-1

--- infra/environments/sandbox/versions.tf
+++ infra/environments/sandbox/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.10.0, < 2.0.0"
 
   backend "s3" {
-    bucket       = "cdo08-tf-state-894597652722-us-west-2"
+    bucket       = "cdo08-tf-state-894597652722-us-east-1"
     key          = "cdo08/sandbox/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
